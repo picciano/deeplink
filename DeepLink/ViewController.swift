0a74948f812de96e27e81f1824d67ebc97294431
Anthony Picciano <picciano@miskander3167.encore.corp.root>
2017-07-07 10:01:11 -0600

    Add support for query string.

--- DeepLink/ViewController.swift
+++ DeepLink/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var schemeField: UITextField!
     @IBOutlet weak var hostField: UITextField!
     @IBOutlet weak var pathField: UITextField!
+    @IBOutlet weak var queryField: UITextField!
 
     @IBAction func openAction(_ sender: Any) {
         var urlString = String()
@@ -27,6 +28,10 @@ class ViewController: UIViewController {
         
         if let path = pathField.text {
             urlString.append("/\(path)")
+        }
+        
+        if let query = queryField.text {
+            urlString.append(query)
         }
         
         if let url = URL(string: urlString) {
